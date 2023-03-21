a0abb3a0d9f2e0eaa6b06f9f598751a2e02ed8b4
ddd444g <daiki.nakamura.424@icloud.com>
2023-03-21 19:47:23 +0900

    jsのAPIの複数回読み込みのエラーを、turblinkを無効化することで解消しました。Placeモデルに最低限のバリデーションを追加

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,4 +2,7 @@ class Place < ApplicationRecord
   belongs_to :user
   geocoded_by :name
   after_validation :geocode, if: :name_changed?
+  validates :name, presence: true
+  validates :latitude, presence: true
+  validates :longitude, presence: true
 end
