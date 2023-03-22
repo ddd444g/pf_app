aeeccb1918662da6f4c101b1b0d1d0ca9a013384
ddd444g <daiki.nakamura.424@icloud.com>
2023-03-22 12:04:55 +0900

    エラーメッセージを日本語化。エラーメッセージを新規作成

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,9 +1,9 @@
 class Place < ApplicationRecord
   belongs_to :user
-  belongs_to :gone_place
+  belongs_to :gone_place, optional: true
   geocoded_by :name
   after_validation :geocode, if: :name_changed?
   validates :name, presence: true
-  validates :latitude, presence: true
-  validates :longitude, presence: true
+  validates :latitude, presence: { message: "で検索し位置を指定してください" }
+  validates :longitude, presence: { message: "したい位置にピンを刺してください" }
 end
