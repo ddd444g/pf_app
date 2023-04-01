6b4f493b71a7ec457427bf4c1c87b6a4309336b4
ddd444g <daiki.nakamura.424@icloud.com>
2023-04-01 17:11:06 +0900

    ec2内でエラーが出るため変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,10 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-  config.cache_classes = true
+  
+  # ec2内でエラーが出るため変更
+  # config.cache_classes = true
+  config.cache_classes = false
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
