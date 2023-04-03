a1b0f816a16124eedc87af73a5208924e98d06d0
ddd444g <daiki.nakamura.424@icloud.com>
2023-04-03 18:15:29 +0900

    数字が大きくエラーになるため修正

--- db/migrate/20230325015546_add_index_to_users_email.rb
+++ db/migrate/20230325015546_add_index_to_users_email.rb
@@ -1,5 +1,5 @@
 class AddIndexToUsersEmail < ActiveRecord::Migration[6.1]
   def change
-    add_index :users, :email, unique: true, length: 255
+    add_index :users, :email, unique: true, length: 191
   end
 end
