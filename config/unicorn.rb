040ee1f1dc4233a77189c7522138c54bcd41b101
ddd444g <daiki.nakamura.424@icloud.com>
2023-04-03 22:06:38 +0900

    unicornのパスが間違っていたため修正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,31 +1,33 @@
-$worker  = 2
-$timeout = 30
-$app_dir = "/var/www/rails/pf_app/current"
-$listen  = File.expand_path '/shared/tmp/sockets/.unicorn.sock', $app_dir
-$pid     = File.expand_path '/shared/tmp/pids/unicorn.pid', $app_dir
-$std_log = File.expand_path '/shared/log/unicorn.log', $app_dir
-# set config
-worker_processes  $worker
-working_directory $app_dir
-stderr_path $std_log
-stdout_path $std_log
-timeout $timeout
-listen  $listen
-pid $pid
+worker_processes 2
+working_directory "/var/www/pf_app/current"
+timeout 30
+
+# Listen on a Unix data socket
+listen "/var/www/pf_app/shared/tmp/sockets/.unicorn.sock"
+
+# Path to unicorn PID file (the server's process ID)
+pid "/var/www/pf_app/shared/tmp/pids/unicorn.pid"
+
+stderr_path "/var/www/pf_app/shared/log/unicorn.log"
+stdout_path "/var/www/pf_app/shared/log/unicorn.log"
+
 # loading booster
 preload_app true
+
 # before starting processes
 before_fork do |server, worker|
   defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
   old_pid = "#{server.config[:pid]}.oldbin"
   if old_pid != server.pid
     begin
-      Process.kill "QUIT", File.read(old_pid).to_i
+      Process.kill("QUIT", File.read(old_pid).to_i)
     rescue Errno::ENOENT, Errno::ESRCH
     end
   end
 end
+
 # after finishing processes
 after_fork do |server, worker|
   defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
 end
+
