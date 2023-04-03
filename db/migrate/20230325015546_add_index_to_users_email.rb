fbf58012460e44035a9872020f0787afc4675027
ddd444g <daiki.nakamura.424@icloud.com>
2023-04-03 18:10:05 +0900

    ec2内でエラーのため編集

--- db/migrate/20230325015546_add_index_to_users_email.rb
+++ db/migrate/20230325015546_add_index_to_users_email.rb
@@ -1,5 +1,5 @@
 class AddIndexToUsersEmail < ActiveRecord::Migration[6.1]
   def change
-    add_index :users, :email, unique: true
+    add_index :users, :email, unique: true, length: 255
   end
 end
