b0b549d0ec09e38f048a95c9bf412bb8b74f9e6e
ddd444g <daiki.nakamura.424@icloud.com>
2023-04-05 19:24:49 +0900

    sshのエラーが出るため追加

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
+set :ssh_options, keys: '~/.ssh/pf_app_2.pem'
 # server '35.79.128.64', user: 'ec2-user', roles: %w{app db web}
 server 'gone-place.com', user: 'ec2-user', roles: %w{app db web}
 
