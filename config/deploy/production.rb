87662ccb48dcdac5fff5153f8134f542bd5864b1
ddd444g <daiki.nakamura.424@icloud.com>
2023-04-05 19:31:57 +0900

    自動デプロイができなくなったため修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,9 +7,9 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-set :ssh_options, keys: '~/.ssh/pf_app_2.pem'
-# server '35.79.128.64', user: 'ec2-user', roles: %w{app db web}
-server 'gone-place.com', user: 'ec2-user', roles: %w{app db web}
+# set :ssh_options, keys: '~/.ssh/pf_app_2.pem'
+server '35.79.128.64', user: 'ec2-user', roles: %w{app db web}
+# server 'gone-place.com', user: 'ec2-user', roles: %w{app db web}
 
 # role-based syntax
 # ==================
