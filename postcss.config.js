feefa2b6288007561f97267bc17d2052bcf81ba0
ddd444g <daiki.nakamura.424@icloud.com>
2023-04-12 00:07:38 +0900

    tailwindを導入.

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   plugins: [
+    // tailwind導入
+    require('tailwindcss')('./app/javascript/css/tailwind.js'),
+    require('autoprefixer'),
+    // ここまで
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
