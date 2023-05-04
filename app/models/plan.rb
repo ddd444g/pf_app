44175df3eaa2bc91527912ce40f7c8f5ae19d29d
ddd444g <daiki.nakamura.424@icloud.com>
2023-05-05 02:55:16 +0900

    planに関するエラーメッセージを日本語化。日付に関するバリデーションを追加。

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -4,4 +4,25 @@ class Plan < ApplicationRecord
   validates :plan_name, presence: true
   validates :start_time, presence: true
   validates :end_time, presence: true
+  validate :date_before_start
+  validate :date_before_end
+  validate :start_same_end
+
+  def date_before_start
+    return if start_time.blank?
+    errors.add(:start_time, "は過去の日付は選択できません") if start_time < Date.today
+  end
+
+  def date_before_end
+    if end_time.blank? || start_time.blank?
+      return
+    end
+    errors.add(:end_time, "は開始日時より後にして下さい") if end_time < start_time
+  end
+
+  def start_same_end
+    if end_time == start_time
+      errors.add(:end_time, "は開始日時より、1分以上後にして下さい")
+    end
+  end
 end
