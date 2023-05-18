1bfaab1198721c4ccb08e0be3b3ab2c9f47a3584
ddd444g <daiki.nakamura.424@icloud.com>
2023-05-18 20:01:10 +0900

    ajaxでplan_placeをcreateするため、登録フォームをalpine.jsを使いモーデルで表示に変更。

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -30,8 +30,8 @@ only: [
   def show
     @user = User.find_by(id: session[:user_id])
     @plan = Plan.find(params[:id])
-    @plan_place = PlanPlace.new
     @plan_places = @plan.plan_places.order(:start_time)
+    @plan_place = PlanPlace.new
   end
 
   def edit
