6079fb4cb66da62f6afb8b9429ecf38f5f41a434
ddd444g <daiki.nakamura.424@icloud.com>
2023-06-19 02:26:42 +0900

    行きたい場所一覧ページに絞り込み検索機能を追加。

--- app/models/place.rb
+++ app/models/place.rb
@@ -12,6 +12,19 @@ class Place < ApplicationRecord
   validates :latitude, presence: { message: "で検索し位置を指定してください" }
   validates :longitude, presence: { message: "したい位置にピンを刺してください" }
 
+  # 絞り込み検索機能
+  scope :search, -> (keyword) {
+    if keyword.present?
+      keywords = keyword.split(/[[:blank:]]+/) # 空白でキーワードを分割
+      search_conditions = keywords.map do |kw|
+        "(name LIKE :kw OR memo LIKE :kw OR googlemap_name LIKE :kw OR address LIKE :kw)"
+      end .join(" OR ")
+      where(search_conditions, keywords.map { |kw| { kw: "%#{kw}%" } }.reduce({}, :merge))
+    else
+      all
+    end
+  }
+
   # 並び替え機能
   scope :latest, -> { order(created_at: :desc) }
   scope :old, -> { order(created_at: :asc) }
