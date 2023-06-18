7068b8fcdf2c0a4298515b87d90fb07cf420419e
ddd444g <daiki.nakamura.424@icloud.com>
2023-06-18 13:53:37 +0900

    行きたい場所一覧画面に並び替え機能を追加。

--- app/models/place.rb
+++ app/models/place.rb
@@ -11,4 +11,22 @@ class Place < ApplicationRecord
   validates :name, presence: true
   validates :latitude, presence: { message: "で検索し位置を指定してください" }
   validates :longitude, presence: { message: "したい位置にピンを刺してください" }
+
+  # 並び替え機能
+  scope :latest, -> { order(created_at: :desc) }
+  scope :old, -> { order(created_at: :asc) }
+  scope :rating, -> { order(rating: :desc) }
+
+  def self.sort_places(sort_param)
+    case sort_param
+    when "latest"
+      latest
+    when "old"
+      old
+    when "rating"
+      rating
+    else
+      all
+    end
+  end
 end
