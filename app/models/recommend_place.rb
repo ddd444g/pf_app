ce2d3ed992aacf4c29f8072b37d88fc7d25413a3
ddd444g <daiki.nakamura.424@icloud.com>
2023-06-20 22:30:27 +0900

    おすすめ一覧も住所とgooglemapでの名前で検索できるようにrecommend_placesテーブルにgooglemapの情報を保存するカラムを追加。

--- app/models/recommend_place.rb
+++ app/models/recommend_place.rb
@@ -11,7 +11,7 @@ class RecommendPlace < ApplicationRecord
     if keyword.present?
       keywords = keyword.split(/[[:blank:]]+/) # 空白でキーワードを分割
       search_conditions = keywords.map do |kw|
-        "(recommend_place_name LIKE :kw OR recommend_comment LIKE :kw)"
+        "(recommend_place_name LIKE :kw OR recommend_comment LIKE :kw OR googlemap_name LIKE :kw OR address LIKE :kw)"
       end.join(" OR ")
       where(search_conditions, keywords.map { |kw| { kw: "%#{kw}%" } }.reduce({}, :merge))
     else
@@ -22,7 +22,7 @@ class RecommendPlace < ApplicationRecord
   # 並び替え機能
   scope :latest, -> { order(created_at: :desc) }
   scope :old, -> { order(created_at: :asc) }
-  scope :rating, -> { joins(:gone_place).order('gone_places.rating DESC') }
+  scope :rating, -> { order(rating: :desc) }
 
   def self.sort_recommend_places(sort_param)
     case sort_param
