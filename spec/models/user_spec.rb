96a510012a5278471d53e5cb6d9dd5f7e56dd93c
ddd444g <daiki.nakamura.424@icloud.com>
2023-09-04 10:23:35 +0900

    contextのタイポとテキストを修正。emailのバリデーションのテストを追加。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe User, type: :model do
   let!(:user) { create(:user) }
 
   describe 'User' do
-    it 'name,enail,passwordがあれば有効な状態であること' do
+    it 'name,email,passwordがあれば有効な状態であること' do
       expect(user).to be_valid
     end
   end
 
   describe 'Userモデルのバリデーションが有効であるか' do
-    context 'nameに対するバリデーション' do
+    context 'nameに対するバリデーションが有効であるか' do
       it 'nameがnilなら登録できないこと' do
         user = build(:user, name: nil)
         user.valid?
@@ -21,6 +21,45 @@ RSpec.describe User, type: :model do
         user = build(:user, name: ' ')
         user.valid?
         expect(user.errors[:name]).to include('を入力してください')
+      end
+    end
+
+    context 'emailに対するバリデーションが有効であるか' do
+      it 'emailがnilなら登録できないこと' do
+        user = build(:user, email: nil)
+        user.valid?
+        expect(user.errors[:email]).to include('を入力してください')
+      end
+
+      it 'emailが空なら登録できないこと' do
+        user = build(:user, email:' ')
+        user.valid?
+        expect(user.errors[:email]).to include('を入力してください')
+      end
+
+      it '重複したemailが存在する場合登録できないこと' do
+        user = create(:user, email:'test@test.jp')
+        email_test_user = build(:user, email:user.email)
+        email_test_user.valid?
+        expect(email_test_user.errors[:email]).to include('はすでに存在します')
+      end
+
+      it '@がなければ登録できないこと' do
+        user = build(:user, email:'test.jp')
+        user.valid?
+        expect(user.errors[:email]).to include('は不正な値です')
+      end
+
+      it 'スペースがあると登録できないこと' do
+        user = build(:user, email:'test @test.jp')
+        user.valid?
+        expect(user.errors[:email]).to include('は不正な値です')
+      end
+
+      it '.で終わっている場合登録できないこと' do
+        user = build(:user, email:'test.test@jp.')
+        user.valid?
+        expect(user.errors[:email]).to include('は不正な値です')
       end
     end
   end
