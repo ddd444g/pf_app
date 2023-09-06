dbc6c1844bb4714fa24c6527d793e2c3df079de0
ddd444g <daiki.nakamura.424@icloud.com>
2023-09-06 10:45:42 +0900

    Placeモデルのテストデータの作り方を修正。登録できるパターンのテストケースを追加。

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -15,43 +15,58 @@ RSpec.describe Place, type: :model do
     let!(:place) { create(:place, user_id: user.id, category_id: category.id) }
     context 'nameに対するバリデーションが有効であるか' do
       it 'nameがnilなら登録できないこと' do
-        place = build(:place, name: nil)
+        place.name = nil
         place.valid?
         expect(place.errors[:name]).to include('を入力してください')
       end
 
       it 'nameが空なら登録できないこと' do
-        place = build(:place, name: ' ')
+        place.name = ' '
         place.valid?
         expect(place.errors[:name]).to include('を入力してください')
+      end
+
+      it 'nameが有効なら登録できること' do
+        place.name = 'test_place'
+        expect(place).to be_valid
       end
     end
 
     context 'latitudeに対するバリデーションが有効であるか' do
-      it 'latitudeが空なら登録できないこと' do
-        place = build(:place, latitude: ' ')
+      it 'latitudeがnilなら登録できないこと' do
+        place.latitude = nil
         place.valid?
         expect(place.errors[:latitude]).to include('で検索し位置を指定してください')
       end
 
-      it 'latitudeがnilなら登録できないこと' do
-        place = build(:place, latitude: nil)
+      it 'latitudeが空なら登録できないこと' do
+        place.latitude = ' '
         place.valid?
         expect(place.errors[:latitude]).to include('で検索し位置を指定してください')
+      end
+
+      it 'latitudeが有効なら登録できること' do
+        place.latitude = 1
+        expect(place).to be_valid
       end
     end
 
     context 'longitudeに対するバリデーションが有効であるか' do
-      it 'longitudeが空なら登録できないこと' do
-        place = build(:place, longitude: ' ')
+      it 'longitudeがnilなら登録できないこと' do
+        place.longitude = nil
         place.valid?
         expect(place.errors[:longitude]).to include('したい位置にピンを刺してください')
       end
 
-      it 'longitudenilがnilなら登録できないこと' do
-        place = build(:place, longitude: nil)
+      it 'longitudeが空なら登録できないこと' do
+        place.longitude = ' '
         place.valid?
         expect(place.errors[:longitude]).to include('したい位置にピンを刺してください')
+      end
+
+      it 'longitudeが有効なら登録できること' do
+        place.longitude = 1
+        expect(place).to be_valid
       end
     end
   end
