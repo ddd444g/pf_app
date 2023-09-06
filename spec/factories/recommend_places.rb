58db194838e6fa1583a124d260aba2319bce27b7
ddd444g <daiki.nakamura.424@icloud.com>
2023-09-06 11:50:41 +0900

    recommend_placeのモデルスペックのテストを追加。nameとcommentのバリデーションのテストまで追加。

--- spec/factories/recommend_places.rb
+++ spec/factories/recommend_places.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :recommend_place do
     recommend_place_name { "MyString" }
-    recommend_comment { "MyString" }
-    user_id { 1 }
-    gone_place_id { 1 }
+    recommend_comment { "test_comment" }
+    googlemap_name { "test_googlemap_name" }
+    address { "test_address" }
+    website { "https.test_website.jp" }
   end
 end
--- spec/models/recommend_place_spec.rb
+++ spec/models/recommend_place_spec.rb
@@ -1,4 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe RecommendPlace, type: :model do
+  let!(:user) { create(:user) }
+  let!(:category) { create(:category) }
+  describe 'RecommendPlaceモデルが登録できるか' do
+    let!(:recommend_place) { create(:recommend_place, user_id: user.id, category_id: category.id) }
+    it 'すべての項目があれば有効な状態であること' do
+      expect(recommend_place).to be_valid
+    end
+  end
+
+  describe 'RecommendPlaceモデルのバリデーションが有効であるか' do
+    let!(:recommend_place) { create(:recommend_place, user_id: user.id, category_id: category.id) }
+    context 'recommend_place_nameに対するバリデーションが有効であるか' do
+      it 'recommend_place_nameがnilなら登録できないこと' do
+        recommend_place.recommend_place_name = nil
+        recommend_place.valid?
+        expect(recommend_place.errors[:recommend_place_name]).to include('を入力してください')
+      end
+
+      it 'recommend_place_nameが空なら登録できないこと' do
+        recommend_place.recommend_place_name = ' '
+        recommend_place.valid?
+        expect(recommend_place.errors[:recommend_place_name]).to include('を入力してください')
+      end
+
+      it 'recommend_place_nameが有効なら登録できること' do
+        recommend_place.recommend_place_name = 'test_recommend_place'
+        expect(recommend_place).to be_valid
+      end
+    end
+
+    context 'recommend_commentに対するバリデーションが有効であるか' do
+      it 'recommend_commentがnilなら登録できないこと' do
+        recommend_place.recommend_comment = nil
+        recommend_place.valid?
+        expect(recommend_place.errors[:recommend_comment]).to include('を入力してください')
+      end
+
+      it 'recommend_commentが空なら登録できないこと' do
+        recommend_place.recommend_comment = ' '
+        recommend_place.valid?
+        expect(recommend_place.errors[:recommend_comment]).to include('を入力してください')
+      end
+
+      it 'recommend_commentが有効なら登録できること' do
+        recommend_place.recommend_comment = 'test_recommend_place'
+        expect(recommend_place).to be_valid
+      end
+    end
+  end
 end
