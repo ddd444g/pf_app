a9d71a4ff7fa54e36366eb1eccbcd17290aa518c
ddd444g <daiki.nakamura.424@icloud.com>
2023-09-06 10:29:52 +0900

    Userモデルのテストデータの作り方を修正。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,27 +12,32 @@ RSpec.describe User, type: :model do
   describe 'Userモデルのバリデーションが有効であるか' do
     context 'nameに対するバリデーションが有効であるか' do
       it 'nameがnilなら登録できないこと' do
-        user = build(:user, name: nil)
+        user.name = nil
         user.valid?
         expect(user.errors[:name]).to include('を入力してください')
       end
 
       it 'nameが空なら登録できないこと' do
-        user = build(:user, name: ' ')
+        user.name = ' '
         user.valid?
         expect(user.errors[:name]).to include('を入力してください')
+      end
+
+      it 'nameがあれば登録できること' do
+        user.name = 'taro'
+        expect(user).to be_valid
       end
     end
 
     context 'emailに対するバリデーションが有効であるか' do
       it 'emailがnilなら登録できないこと' do
-        user = build(:user, email: nil)
+        user.email = nil
         user.valid?
         expect(user.errors[:email]).to include('を入力してください')
       end
 
       it 'emailが空なら登録できないこと' do
-        user = build(:user, email: ' ')
+        user.email = ' '
         user.valid?
         expect(user.errors[:email]).to include('を入力してください')
       end
@@ -45,50 +50,57 @@ RSpec.describe User, type: :model do
       end
 
       it '@がなければ登録できないこと' do
-        user = build(:user, email: 'test.jp')
+        user.email = 'test.jp'
         user.valid?
         expect(user.errors[:email]).to include('は不正な値です')
       end
 
       it 'スペースがあると登録できないこと' do
-        user = build(:user, email: 'test @test.jp')
+        user.email = 'test @test.jp'
         user.valid?
         expect(user.errors[:email]).to include('は不正な値です')
       end
 
       it '.で終わっている場合登録できないこと' do
-        user = build(:user, email: 'test.test@jp.')
+        user.email = 'test.test@jp.'
         user.valid?
         expect(user.errors[:email]).to include('は不正な値です')
+      end
+
+      it '正規表現であれば場合登録できること' do
+        user.email = 'test@test.jp'
+        expect(user).to be_valid
       end
     end
 
     context 'passwordに対するバリデーションが有効であるか' do
       it 'passwordがnilなら登録できないこと' do
-        user = build(:user, password: nil)
+        user.password = nil
         user.valid?
         expect(user.errors[:password]).to include('を入力してください')
       end
 
       it 'passwordが空なら登録できないこと' do
-        user = build(:user, password: ' ')
+        user.password = ' '
         user.valid?
         expect(user.errors[:password]).to include('を入力してください')
       end
 
       it 'passwordが5文字以下なら登録できないこと' do
-        user = build(:user, password: '12345')
+        user.password = '12345'
         user.valid?
         expect(user.errors[:password]).to include('は6文字以上で入力してください')
       end
 
       it 'passwordが6文字以上なら登録できること' do
-        user = build(:user, password: '123456')
+        user.password = '123456'
+        user.password_confirmation = user.password
         expect(user).to be_valid
       end
 
       it 'passwordがpassword_confirmationと一致しない場合登録できないこと' do
-        user = build(:user, password_confirmation: '')
+        user.password = '123456'
+        user.password_confirmation = '654321'
         user.valid?
         expect(user.errors[:password_confirmation]).to include('とパスワードの入力が一致しません')
       end
