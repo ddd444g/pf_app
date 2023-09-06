d4a57ced421828bf785bad67cd842a6e7abaa513
ddd444g <daiki.nakamura.424@icloud.com>
2023-09-07 08:46:00 +0900

    categoryの関連付けのテストを追加。modelsフォルダのcategory.rbのタイポを修正。

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,4 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
+  describe 'Categoryモデルの関連付けが出来ているか' do
+    it 'placesとの関連付けがあること' do
+      should have_many(:places)
+    end
+
+    it 'gone_placesとの関連付けがあること' do
+      should have_many(:gone_places)
+    end
+
+    it 'plan_placesとの関連付けがあること' do
+      should have_many(:plan_places)
+    end
+
+    it 'recommend_placesとの関連付けがあること' do
+      should have_many(:recommend_places)
+    end
+  end
 end
