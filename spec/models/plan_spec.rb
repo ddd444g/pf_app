a7b0b40c484828a3c36ffe4b06b46b54dff9e8d6
ddd444g <daiki.nakamura.424@icloud.com>
2023-09-07 00:40:43 +0900

    planの必須項目のバリデーションのテストを追加。

--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -30,5 +30,47 @@ RSpec.describe Plan, type: :model do
         expect(plan).to be_valid
       end
     end
+
+    context 'start_timeに対するバリデーションが有効であるか' do
+      it 'start_timeがnilなら登録できないこと' do
+        plan.start_time = nil
+        plan.valid?
+        expect(plan.errors[:start_time]).to include('を入力してください')
+      end
+
+      it 'start_timeが空なら登録できないこと' do
+        plan.start_time = ' '
+        plan.valid?
+        expect(plan.errors[:start_time]).to include('を入力してください')
+      end
+    end
+
+    context 'end_timeに対するバリデーションが有効であるか' do
+      it 'end_timeがnilなら登録できないこと' do
+        plan.end_time = nil
+        plan.valid?
+        expect(plan.errors[:end_time]).to include('を入力してください')
+      end
+
+      it 'end_timeが空なら登録できないこと' do
+        plan.end_time = ' '
+        plan.valid?
+        expect(plan.errors[:end_time]).to include('を入力してください')
+      end
+    end
+
+    context 'plan_colorに対するバリデーションが有効であるか' do
+      it 'plan_colorがnilなら登録できないこと' do
+        plan.plan_color = nil
+        plan.valid?
+        expect(plan.errors[:plan_color]).to include('を入力してください')
+      end
+
+      it 'plan_colorが空なら登録できないこと' do
+        plan.plan_color = ' '
+        plan.valid?
+        expect(plan.errors[:plan_color]).to include('を入力してください')
+      end
+    end
   end
 end
