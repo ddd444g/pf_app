0a43e018d707b97714e8d7c8ff56a4168897fc0c
ddd444g <daiki.nakamura.424@icloud.com>
2023-09-06 10:11:33 +0900

    テストデータの作り方が間違っていたため修正。Place,Userモデルのテストも今後修正します。scoreの境界線テストを追加。

--- spec/models/gone_place_spec.rb
+++ spec/models/gone_place_spec.rb
@@ -15,13 +15,13 @@ RSpec.describe GonePlace, type: :model do
     let!(:gone_place) { create(:gone_place, user_id: user.id, category_id: category.id) }
     context 'nameに対するバリデーションが有効であるか' do
       it 'nameがnilなら登録できないこと' do
-        gone_place = build(:gone_place, name: nil)
+        gone_place.name = nil
         gone_place.valid?
         expect(gone_place.errors[:name]).to include('を入力してください')
       end
 
       it 'nameが空なら登録できないこと' do
-        gone_place = build(:gone_place, name: ' ')
+        gone_place.name = ' '
         gone_place.valid?
         expect(gone_place.errors[:name]).to include('を入力してください')
       end
@@ -29,13 +29,13 @@ RSpec.describe GonePlace, type: :model do
 
     context 'latitudeに対するバリデーションが有効であるか' do
       it 'latitudeが空なら登録できないこと' do
-        gone_place = build(:gone_place, latitude: ' ')
+        gone_place.latitude = ' '
         gone_place.valid?
         expect(gone_place.errors[:latitude]).to include('で検索し位置を指定してください')
       end
 
       it 'latitudeがnilなら登録できないこと' do
-        gone_place = build(:gone_place, latitude: nil)
+        gone_place.latitude = nil
         gone_place.valid?
         expect(gone_place.errors[:latitude]).to include('で検索し位置を指定してください')
       end
@@ -43,13 +43,13 @@ RSpec.describe GonePlace, type: :model do
 
     context 'longitudeに対するバリデーションが有効であるか' do
       it 'longitudeが空なら登録できないこと' do
-        gone_place = build(:gone_place, longitude: ' ')
+        gone_place.longitude = ' '
         gone_place.valid?
         expect(gone_place.errors[:longitude]).to include('したい位置にピンを刺してください')
       end
 
       it 'longitudenilがnilなら登録できないこと' do
-        gone_place = build(:gone_place, longitude: nil)
+        gone_place.longitude = nil
         gone_place.valid?
         expect(gone_place.errors[:longitude]).to include('したい位置にピンを刺してください')
       end
@@ -57,13 +57,13 @@ RSpec.describe GonePlace, type: :model do
 
     context 'reviewに対するバリデーションが有効であるか' do
       it 'reviewが空なら登録できないこと' do
-        gone_place = build(:gone_place, review: ' ')
+        gone_place.review = ' '
         gone_place.valid?
         expect(gone_place.errors[:review]).to include('を入力してください')
       end
 
       it 'reviewがnilなら登録できないこと' do
-        gone_place = build(:gone_place, review: nil)
+        gone_place.review = nil
         gone_place.valid?
         expect(gone_place.errors[:review]).to include('を入力してください')
       end
@@ -71,37 +71,47 @@ RSpec.describe GonePlace, type: :model do
 
     context 'scoreに対するバリデーションが有効であるか' do
       it 'scoreが空なら登録できないこと' do
-        gone_place = build(:gone_place, score: " ")
+        gone_place.score = ' '
         gone_place.valid?
         expect(gone_place.errors[:score]).to include('を入力してください')
       end
 
       it 'scoreがnilなら登録できないこと' do
-        gone_place = build(:gone_place, score: nil)
+        gone_place.score = nil
         gone_place.valid?
         expect(gone_place.errors[:score]).to include('を入力してください')
       end
 
       it 'scoreが1未満なら登録できないこと' do
-        gone_place = build(:gone_place, score: 0)
+        gone_place.score = 0
         gone_place.valid?
         expect(gone_place.errors[:score]).to include('は1以上の値にしてください')
       end
 
+      it 'scoreが1以上なら登録できること' do
+        gone_place.score = 1
+        expect(gone_place).to be_valid
+      end
+
       it 'scoreが11以上なら登録できないこと' do
-        gone_place = build(:gone_place, score: 11)
+        gone_place.score = 11
         gone_place.valid?
         expect(gone_place.errors[:score]).to include('は10以下の値にしてください')
       end
 
+      it 'scoreが10以下なら登録できること' do
+        gone_place.score = 10
+        expect(gone_place).to be_valid
+      end
+
       it 'scoreが小数点を含む場合は登録できないこと' do
-        gone_place = build(:gone_place, score: 5.5)
+        gone_place.score = 5.5
         gone_place.valid?
         expect(gone_place.errors[:score]).to include('は整数で入力してください')
       end
 
       it 'scoreが全角数字の場合は登録できないこと' do
-        gone_place = build(:gone_place, score: '５')
+        gone_place.score = '５'
         gone_place.valid?
         expect(gone_place.errors[:score]).to include('は数値で入力してください')
       end
