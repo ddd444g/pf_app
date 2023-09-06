ff63cec88585ee5b6a360c33101e852da216d506
ddd444g <daiki.nakamura.424@icloud.com>
2023-09-07 00:22:31 +0900

    Planモデルのテストデータを追加。登録できるパターンだけ追加。

--- spec/factories/plans.rb
+++ spec/factories/plans.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :plan do
-    plan_name { "MyString" }
+    plan_name { "test_name" }
     start_time { "2023-04-28 07:42:57" }
     end_time { "2023-12-28 07:42:57" }
     plan_color { "red" }
--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Plan, type: :model do
+  let!(:user) { create(:user) }
+
+  describe 'Planモデルが登録できるか' do
+    let!(:plan) { create(:plan, user_id: user.id) }
+    it 'すべての項目があれば有効な状態であること' do
+      expect(plan).to be_valid
+    end
+  end
 end
