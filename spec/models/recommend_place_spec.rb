198140fd53bca13a238583463b53e99a3083fee5
ddd444g <daiki.nakamura.424@icloud.com>
2023-09-06 13:21:39 +0900

    recommend_placeの絞り込み検索のテストを追加。

--- spec/models/recommend_place_spec.rb
+++ spec/models/recommend_place_spec.rb
@@ -50,4 +50,170 @@ RSpec.describe RecommendPlace, type: :model do
       end
     end
   end
+
+  describe '絞り込み検索が機能しているか' do
+    let!(:tokyo) do
+      create(:recommend_place, user_id: user.id, category_id: category.id, recommend_place_name: 'tokyo',
+                               recommend_comment: 'Kanto',
+                               googlemap_name: 'TOKYO',
+                               address: 'japan-tokyo')
+    end
+    let!(:tokyo2) do
+      create(:recommend_place, user_id: user.id, category_id: category.id, recommend_place_name: 'tokyo2',
+                               recommend_comment: 'Kanto',
+                               googlemap_name: 'TOKYO2',
+                               address: 'japan-tokyo2')
+    end
+    let!(:osaka) do
+      create(:recommend_place, user_id: user.id, category_id: category.id, recommend_place_name: 'osaka',
+                               recommend_comment: 'Kansai',
+                               googlemap_name: 'OSAKA',
+                               address: 'japan-osaka')
+    end
+
+    context 'キーワードが空白またはnilの場合' do
+      it 'キーワードがnilの場合、全てのモデルを返すこと' do
+        results = RecommendPlace.search(nil)
+        expect(results.count).to eq(3)
+      end
+
+      it 'キーワードがnilの場合、tokyo,tokyo2,osakaが検索結果に含まれていること' do
+        results = RecommendPlace.search(nil)
+        expect(results).to include(tokyo, tokyo2, osaka)
+      end
+
+      it 'キーワードが空白の場合、全てのモデルを返すこと' do
+        results = RecommendPlace.search(" ")
+        expect(results.count).to eq(3)
+      end
+
+      it 'キーワードが空白の場合、tokyo,tokyo2,osakaが検索結果に含まれていること' do
+        results = RecommendPlace.search(" ")
+        expect(results).to include(tokyo, tokyo2, osaka)
+      end
+    end
+
+    context 'recommned_place_nameで検索する場合' do
+      it '該当する1件が取得できていること' do
+        results = RecommendPlace.search("tokyo2")
+        expect(results.count).to eq(1)
+      end
+
+      it '該当するtokyo2が取得できていること' do
+        results = RecommendPlace.search("tokyo2")
+        expect(results).to include(tokyo2)
+      end
+
+      it '部分一致で該当する2件が取得できていること' do
+        results = RecommendPlace.search("to")
+        expect(results.count).to eq(2)
+      end
+
+      it '部分一致で該当するtokyo,tokyo2が取得できていること' do
+        results = RecommendPlace.search("to")
+        expect(results).to include(tokyo, tokyo2)
+      end
+
+      it '該当しない場合、何も取得しないこと' do
+        results = RecommendPlace.search("fukuoka")
+        expect(results.count).to eq(0)
+      end
+    end
+
+    context 'recommend_commentで検索する場合' do
+      it '該当する1件が取得できていること' do
+        results = RecommendPlace.search("Kansai")
+        expect(results.count).to eq(1)
+      end
+
+      it '該当するosakaが取得できていること' do
+        results = RecommendPlace.search("Kansai")
+        expect(results).to include(osaka)
+      end
+
+      it '部分一致で該当する1件が取得できていること' do
+        results = RecommendPlace.search("Kans")
+        expect(results.count).to eq(1)
+      end
+
+      it '部分一致で該当するosakaが取得できていること' do
+        results = RecommendPlace.search("Kans")
+        expect(results).to include(osaka)
+      end
+
+      it '部分一致で該当する2件が取得できていること' do
+        results = RecommendPlace.search("Kant")
+        expect(results.count).to eq(2)
+      end
+
+      it '部分一致で該当するtokyo,tokyo2が取得できていること' do
+        results = RecommendPlace.search("kant")
+        expect(results).to include(tokyo, tokyo2)
+      end
+    end
+
+    context 'googlemap_nameで検索する場合' do
+      it '該当する1件が取得できていること' do
+        results = RecommendPlace.search("TOKYO2")
+        expect(results.count).to eq(1)
+      end
+
+      it '該当するtokyo2が取得できていること' do
+        results = RecommendPlace.search("TOKYO2")
+        expect(results).to include(tokyo2)
+      end
+
+      it '部分一致で該当する1件が取得できていること' do
+        results = RecommendPlace.search("OSA")
+        expect(results.count).to eq(1)
+      end
+
+      it '部分一致で該当するosakaが取得できていること' do
+        results = RecommendPlace.search("OSA")
+        expect(results).to include(osaka)
+      end
+
+      it '部分一致で該当する2件が取得できていること' do
+        results = RecommendPlace.search("TOK")
+        expect(results.count).to eq(2)
+      end
+
+      it '部分一致で該当するtokyo,tokyo2が取得できていること' do
+        results = RecommendPlace.search("TOK")
+        expect(results).to include(tokyo, tokyo2)
+      end
+    end
+
+    context 'addressで検索する場合' do
+      it '該当する1件が取得できていること' do
+        results = RecommendPlace.search("japan-tokyo2")
+        expect(results.count).to eq(1)
+      end
+
+      it '該当するtokyo2が取得できていること' do
+        results = RecommendPlace.search("japan-tokyo2")
+        expect(results).to include(tokyo2)
+      end
+
+      it '部分一致で該当する1件が取得できていること' do
+        results = RecommendPlace.search("japan-o")
+        expect(results.count).to eq(1)
+      end
+
+      it '部分一致で該当するosakaが取得できていること' do
+        results = RecommendPlace.search("japan-o")
+        expect(results).to include(osaka)
+      end
+
+      it '部分一致で該当する2件が取得できていること' do
+        results = RecommendPlace.search("japan-t")
+        expect(results.count).to eq(2)
+      end
+
+      it '部分一致で該当するtokyo,tokyo2が取得できていること' do
+        results = RecommendPlace.search("japan-t")
+        expect(results).to include(tokyo, tokyo2)
+      end
+    end
+  end
 end
