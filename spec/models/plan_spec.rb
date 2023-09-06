fd227cb70745c9d826aceb89944fc5502c4413ec
ddd444g <daiki.nakamura.424@icloud.com>
2023-09-07 00:27:40 +0900

    plan_nameに対するバリデーションのテストを追加。

--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -9,4 +9,26 @@ RSpec.describe Plan, type: :model do
       expect(plan).to be_valid
     end
   end
+
+  describe 'planモデルのバリデーションが有効であるか' do
+    let!(:plan) { create(:plan, user_id: user.id) }
+    context 'plan_nameに対するバリデーションが有効であるか' do
+      it 'nameがnilなら登録できないこと' do
+        plan.plan_name = nil
+        plan.valid?
+        expect(plan.errors[:plan_name]).to include('を入力してください')
+      end
+
+      it 'plan_nameが空なら登録できないこと' do
+        plan.plan_name = ' '
+        plan.valid?
+        expect(plan.errors[:plan_name]).to include('を入力してください')
+      end
+
+      it 'plan_nameが有効なら登録できること' do
+        plan.plan_name = 'test_plan'
+        expect(plan).to be_valid
+      end
+    end
+  end
 end
