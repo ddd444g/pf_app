e9fc98549af5c65906d42321404cff3bc27772a6
ddd444g <daiki.nakamura.424@icloud.com>
2023-09-07 19:28:49 +0900

    userのシステムテストを追加。新規登録成功のみ追加。ログインと同じように、新規登録後もpalaces/indexページに移動するように修正。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
     if @user.save
       session[:user_id] = @user.id
       flash[:notice] = 'ユーザーを新規登録しました'
-      redirect_to user_path(@user)
+      redirect_to places_path
     else
       render 'new'
     end
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -3,14 +3,40 @@ require 'rails_helper'
 RSpec.describe 'Users_system', type: :system do
   let!(:user) { create(:user) }
 
-  before do
-    visit login_path
-    fill_in "メールアドレス", with: user.email
-    fill_in "パスワード", with: user.password
-    click_button "ログインする"
+  describe 'ユーザー新規登録' do
+    before do
+      visit new_user_path
+    end
+
+    context 'フォームの入力値が正常の場合' do
+      it 'ユーザーの新規作成が成功し行きたい場所一覧ページに移動すること' do
+        fill_in '名前', with: user.name
+        fill_in 'メールアドレス', with: "test@test.com"
+        fill_in 'パスワード(6文字以上)', with: "123456"
+        fill_in '確認用パスワード', with: "123456"
+        click_button '入力を完了する'
+        expect(current_path).to eq places_path
+      end
+    end
+
+    context '名前未入力' do
+    end
+
+    context 'メールアドレス未入力' do
+    end
+
+    context '登録済メールアドレス' do
+    end
   end
 
-  describe 'user一覧の情報が表示されてるか' do
+  describe 'userの情報が表示されてるか' do
+    before do
+      visit login_path
+      fill_in 'メールアドレス', with: user.email
+      fill_in 'パスワード', with: user.password
+      click_button 'ログインする'
+    end
+
     it 'nameが表示されていること' do
       expect(page).to have_content user.name
     end
