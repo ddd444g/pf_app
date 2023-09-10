e4f3f0cbe8ea560ddf8c0d58174c5274bc6c0347
ddd444g <daiki.nakamura.424@icloud.com>
2023-09-11 05:42:52 +0900

    mapで検索していない場合のテストを追加。事前準備をbeforeの中にまとめました。

--- spec/system/places_spec.rb
+++ spec/system/places_spec.rb
@@ -11,10 +11,13 @@ RSpec.describe 'Places_system', type: :system do
   end
 
   describe 'Place新規登録', js: true do
+    before do
+      # モーダルを開く
+      find_by_id('create').click
+    end
+
     context 'フォームの入力値が正常の場合' do
       it 'Placeの新規作成が成功し新規作成した場所が表示されていること' do
-        # モーダルを開く
-        find_by_id('create').click
         # mapで検索
         page.execute_script("document.getElementById('address').value = 'sapporo-station'")
         find_by_id('search-button').click
@@ -27,6 +30,17 @@ RSpec.describe 'Places_system', type: :system do
         click_button '登録を完了する'
         expect(page).to have_content 'sapporo-station'
         expect(page).to have_content 'others'
+      end
+    end
+
+    context 'mapで検索してない場合' do
+      it 'latitude,longitudeのバリデーションでひっかりエラーメッセージが表示されること' do
+        fill_in '登録名', with: 'sapporo-station'
+        fill_in 'memo', with: 'Hokkaido'
+        select('others', from: 'place_category_id')
+        click_button '登録を完了する'
+        expect(page).to have_content 'mapで検索し位置を指定してください'
+        expect(page).to have_content '登録したい位置にピンを刺してください'
       end
     end
   end
