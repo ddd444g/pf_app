d5f4331f7cda75948b96df60e1fda9a075ad6398
ddd444g <daiki.nakamura.424@icloud.com>
2023-09-10 19:44:34 +0900

    テストが見にくいので、factorybotで作成したデータでテストを実行。

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.create!(name: "宿泊施設")
+Category.create!(name: "飲食店")
+Category.create!(name: "アミューズメントパーク")
+Category.create!(name: "その他")
 
-if Rails.env.development? || Rails.env.production?
-  Category.create!(name: "宿泊施設")
-  Category.create!(name: "飲食店")
-  Category.create!(name: "アミューズメントパーク")
-  Category.create!(name: "その他")
-end
+# if Rails.env.development? || Rails.env.production?
+#   Category.create!(name: "宿泊施設")
+#   Category.create!(name: "飲食店")
+#   Category.create!(name: "アミューズメントパーク")
+#   Category.create!(name: "その他")
+# end
 
-if Rails.env.test?
-  Category.create!(name: "hotel")
-  Category.create!(name: "restaurant")
-  Category.create!(name: "amusement-park")
-  Category.create!(name: "others")
-end
+# if Rails.env.test?
+#   Category.create!(name: "hotel")
+#   Category.create!(name: "restaurant")
+#   Category.create!(name: "amusement-park")
+#   Category.create!(name: "others")
+# end
