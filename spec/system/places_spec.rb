7af7e80428364acae1d52e6cfb4c7ad534b31e58
ddd444g <daiki.nakamura.424@icloud.com>
2023-09-10 19:49:01 +0900

    circleciが通らないため、元に戻しました。

--- spec/system/places_spec.rb
+++ spec/system/places_spec.rb
@@ -11,10 +11,6 @@ RSpec.describe 'Places_system', type: :system do
   end
 
   describe 'Place新規登録', js: true do
-    let!(:category1) { create(:category, name: 'hotel') }
-    let!(:category2) { create(:category, name: 'restaurant') }
-    let!(:category3) { create(:category, name: 'amusement-park') }
-    let!(:category4) { create(:category, name: 'others') }
     context 'フォームの入力値が正常の場合' do
       it 'Placeの新規作成が成功し新規作成した場所が表示されていること' do
         # モーダルを開く
@@ -27,7 +23,7 @@ RSpec.describe 'Places_system', type: :system do
         # 自動設定されたのを自分で上書き
         fill_in '登録名', with: 'sapporo-station'
         fill_in 'memo', with: 'Hokkaido'
-        select(category4.name, from: 'place_category_id')
+        select('others', from: 'place_category_id')
         click_button '登録を完了する'
         expect(page).to have_content 'sapporo-station'
         expect(page).to have_content 'others'
